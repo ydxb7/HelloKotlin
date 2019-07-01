21d09027de46a2ab54ff1a1c45694460f6a226d2
Yini <ydxb7@mail.missouri.edu>
2019-07-01 03:35:52 -0700

    let apply run with

--- src/Aquarium/Lambda.kt
+++ src/Aquarium/Lambda.kt
@@ -4,13 +4,47 @@ fun main(args: Array<String>) {
 
     { println("Hello Fish") }()
 
-    data class Fish(val name: String)
+    data class Fish(var name: String)
 
-    val myFish = listOf(Fish("Flipper"), Fish("Moby Dick"), Fish("Dory"))
+    val fish = Fish("splashy")
 
-    myFish.filter { it.name.contains("i") }.joinToString(" ") { it.name }
+    with(fish.name) {
+        capitalize()  // Splashy
+    }
 
-    val symptoms = mutableListOf("white spots", "red spots", "not eating", "bloated", "belly up")
-    symptoms.joinToString("!") { it }    // white spots!red spots!not eating!bloated!belly up
+    myWith(fish.name) {
+        println(capitalize())
+    }
+
+
+
+    println("run:")
+    println(fish.run { name.capitalize() })   // Splashy
+    println(fish.run { name.capitalize() }.run { this + "fish" })   // Splashyfish
+
+//    println(fish.apply { name = name.capitalize() })  // Fish(name=Splashy)
+
+    println("let:")
+
+    println(fish.let { it.name.capitalize() })    // Splashy
+    println(fish.let { it.name.capitalize() }.let { it + "fish" })   // Splashyfish
+    println(fish.let { it.name.capitalize() }.let { it + "fish" }.let { it.length })   // 11
+    println(fish.let { it.name.capitalize() }.let { it + "fish" }.let { it.length }.let { it + 31 })   // 42
+
+
+
+
+//    val myFish = listOf(Fish("Flipper"), Fish("Moby Dick"), Fish("Dory"))
+//
+//    myFish.filter { it.name.contains("i") }.joinToString(" ") { it.name }
+//
+//    val symptoms = mutableListOf("white spots", "red spots", "not eating", "bloated", "belly up")
+//    symptoms.joinToString("!") { it }    // white spots!red spots!not eating!bloated!belly up
 
 }
+
+
+fun myWith(name: String, block: String.() -> Unit) {
+    name.block()
+}
+
